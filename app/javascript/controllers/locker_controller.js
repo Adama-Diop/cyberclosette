a1a140f42ab70ca7a8c7c89c8f06c3ac9801171a
Inès Silvestre <110823086+ines-silvestre@users.noreply.github.com>
2022-12-02 16:50:44 +0100

    loker petit

---
+++ app/javascript/controllers/locker_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log = "Hello World!"
+  }
+
+lock() {
+  $( ".lock" ).click(function() {
+  $(this).toggleClass('unlocked');
+  });
+}
+
+}
